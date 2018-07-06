035ab69082ae129446fa87355556dcd16a5d244a
Mario Grandi <mgrandi@cern.ch>
2018-07-06 23:54:46 +0200

    area & circularity cuts added

--- run/Find_CP.py
+++ run/Find_CP.py
@@ -4,20 +4,63 @@ import cv2
 import numpy as np;
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import math
 
 def OpenImage(name):
-    img = cv2.imread(name,cv2.IMREAD_GRAYSCALE)
+#    img = cv2.imread(name,cv2.IMREAD_GRAYSCALE)
+    imgo = cv2.imread(name)
+    img = cv2.cvtColor(imgo, cv2.COLOR_BGR2GRAY) 
     #blr = cv2.medianBlur(img,5)
-    blr = cv2.blur(img,(10,10))
+    blr = cv2.blur(img,(20,20))
     th = cv2.adaptiveThreshold(blr,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-    null , th2 = cv2.threshold(blr, 30, 255, cv2.THRESH_BINARY_INV) 
-    return img , blr, th , th2
+    null , th2 = cv2.threshold(blr, 45, 255, cv2.THRESH_BINARY_INV) 
+    return imgo ,img, blr, th , th2
 
-F = "/Users/mariograndi/Desktop/Blob_detection/img/4kx4k/20000X_0043.jpeg"
-img , blr, th, th2 = OpenImage(F)
 
-images = [img, blr, th , th2]
-for i in range(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-    plt.xticks([]),plt.yticks([])
+def findContour(thresh):
+    original, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_area = []
+    # calculate area and filter into new array
+    i=0
+    for con in contours:
+        i=i+1
+        area = cv2.contourArea(con)
+        if 320 < area:
+            contours_area.append(con)
+            contours_circles = []
+            # check if contour is of circular shape
+    for con in contours_area:
+        perimeter = cv2.arcLength(con, True)
+        area = cv2.contourArea(con)
+        circularity = 4*math.pi*(area/(perimeter*perimeter))
+        if 0.6 < circularity:
+           # plt.plot(area,circularity,'ro')
+           # plt.plot()
+            contours_circles.append(con)
+    return contours_circles
+
+
+
+
+
+
+
+F = "/Users/mariograndi/Desktop/Blob_detection/img/4kx4k/20000X_0002.jpeg"
+imgo ,img , blr, th, th2 = OpenImage(F)
+images = [imgo, img, blr, th , th2]
+
+cv2.imshow("original",imgo)
+cv2.imshow("blurred",blr)
+cv2.imshow("threshold",th2)
+cont = findContour(th2)
+cv2.drawContours(imgo,cont,-1,(250,100,0),3)
+cv2.imshow("contoured",imgo)
+cv2.waitKey()
+
+#plt.imshow(th2,'gray')
+#for i in range(4):
+#    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+#    plt.xticks([]),plt.yticks([])
 plt.show()
+
+
